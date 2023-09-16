ebd02b990c0d8dcea9f33f7317853e4ae9b2bd38
barbara.andrade <bmattiolyandrade@gmail.com>
2023-09-16 00:21:50 -0300

    chore(code): Criação do projeto spring boot

---
+++ implementacao/lab2_gestao_automoveis/src/main/java/lds/lab2_gestao_automoveis/Lab2GestaoAutomoveisApplication.java
@@ -0,0 +1,13 @@
+package lds.lab2_gestao_automoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2GestaoAutomoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2GestaoAutomoveisApplication.class, args);
+	}
+
+}
---
+++ implementacao/lab2_gestao_automoveis/src/test/java/lds/lab2_gestao_automoveis/Lab2GestaoAutomoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package lds.lab2_gestao_automoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2GestaoAutomoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
