cce266dca646e5d5f7bb868805e8de56148cf4cb
Laura Melo <lauraenisiam@gmail.com>
2023-09-17 18:06:01 -0300

    telas

---
+++ implementacao/lab2_gestao_automoveis/view/src/components/home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export const CadastroDeUsuario = () => {
+    return (
+        <Typography variant="h4" fontWeight="fontWeightMedium" mb={10}>
+        Crie sua conta
+      </Typography>
+    );
+}
