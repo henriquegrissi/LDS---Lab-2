a554f9a1cd946f3c3c80645fe33fb4911f21cba3
barbara.andrade <bmattiolyandrade@gmail.com>
2023-09-17 18:27:00 -0300

    fix(frontend): Adição de dependencias

--- implementacao/lab2_gestao_automoveis/view/src/App.jsx
+++ implementacao/lab2_gestao_automoveis/view/src/App.jsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import { Login } from './components/login';
-import { CadastroDeUsuario } from './components/cadastroUsuario';
-import  {Home}  from './components/home/index';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import  {CadastroDeUsuario}  from './components/home/index';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/usuario/login" element={<Login/>}/>
+        <Route path="/home" element={<CadastroDeUsuario/>}/>
         <Route path="/usuario/cadastro" element={<CadastroDeUsuario/>}/>
       </Routes>
     </BrowserRouter>
-
   )
 }
 
