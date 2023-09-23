fec80dbe85773af5e7e5e3baf87f7b66ce6029f1
Arthur Jansen <artjansentec@gmail.com>
2023-09-23 01:02:22 -0300

    ajustes finos

--- implementacao/lab2_gestao_automoveis/src/main/java/lds_lab2/lab2_gestao_automoveis/dto/VeiculoDto.java
+++ implementacao/lab2_gestao_automoveis/src/main/java/lds_lab2/lab2_gestao_automoveis/dto/VeiculoDto.java
@@ -3,12 +3,12 @@ package lds_lab2.lab2_gestao_automoveis.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record VeiculoDto(
-        @NotBlank Long id,
+        Long id,
         @NotBlank String marca,
         @NotBlank String placa,
         @NotBlank String modelo,
         @NotBlank String ano,
-        @NotBlank boolean alugado
+        boolean alugado
         ) {
 
 }
